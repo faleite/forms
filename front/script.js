289be1603a04a54a1de2f47397431f478eb4a020
Fabricio Leite <fabricio_2310@hotmail.com>
2026-02-09 22:55:45 +0000

    (Update) new js script

--- front/script.js
+++ front/script.js
@@ -4,6 +4,31 @@ document.getElementById("controleDiario").addEventListener("submit", function(ev
 
 	const formData = {
 		name: document.getElementById("name").value,
-
+		matricula: document.getElementById("matricula").value,
+		kmIniciais: document.getElementById("kmIniciais").value,
+		giro: document.getElementById("giro").value,
+		qtdObjetos: document.getElementById("qtdObjetos").value,
+		qtdRecolhas: document.getElementById("qtdRecolhas").value
 	}
+	
+	console.log("Form Data: ", formData);
+	
+	fetch('http://localhost:8080/api/save-data', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(formData)
+	}).then(response => {
+		if (response.ok) {
+			console.log("Form Data submitted: ", formData);
+			document.getElementById("success").innerText = "Data saved successfully";
+			document.getElementById("controleDiario").reset();
+		} else {
+			alert("Error savind data. Please try again.");
+		}
+	}).catch(error => {
+		console.error("Error: ", error);
+		alert("Error savind data. Please try again.");
+	});
 });
